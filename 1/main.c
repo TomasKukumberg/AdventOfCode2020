993f988678751db58a684362ef1fa03b039c7926
Tomas Kukumberg <tomas.kukumberg@centrum.sk>
2020-12-19 07:29:19 +0100

    added makefile

--- 1/main.c
+++ 1/main.c
@@ -4,12 +4,12 @@
 
 int main(char** argv, int argc) {
     int* arr;
-    FILE *fp = fopen("./numbers.txt", "r");
+    FILE *fp = fopen("input.txt", "r");
     int line_cnt;
 
     line_cnt = count_lines(fp);
     load_file(&arr, fp, line_cnt);
-    printf("Result: %d", get_result(arr, line_cnt, 2020));
+    printf("Result: %d\n", get_result(arr, line_cnt, 2020));
     
     fclose(fp);
     free(arr);
